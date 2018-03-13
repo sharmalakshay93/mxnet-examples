9dde7ed2dc21b194208a40bec98a4ce67cb77354
Sharma <slakshay@8c859043214c.ant.amazon.com>
2018-03-12 23:34:07 -0700

    update custom_dataset readme

---
+++ custom_dataset/fmd_to_rec.sh
@@ -0,0 +1,5 @@
+wget https://people.csail.mit.edu/celiu/CVPR2010/FMD/FMD.zip
+unzip FMD.zip
+m="/Users/slakshay/anaconda3/lib/python3.6/site-packages/mxnet"
+python $m/tools/im2rec.py --list --recursive --train-ratio 0.7 fmd image
+python $m/tools/im2rec.py --resize 240 --quality 95 --num-thread 16 fmd image
